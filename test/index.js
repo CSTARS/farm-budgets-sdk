15b86c92654fe1f4ce894793c11900c172ec90b7
Justin Merz <jrmerz@gmail.com>
2016-01-08 15:45:39 -0800

    budget fixes

--- test/index.js
+++ test/index.js
@@ -12,4 +12,27 @@ sdk.login(function(resp){
 function onBudgetLoad(budget) {
   console.log(budget.getName());
   console.log(sdk.getTotal());
+
+  // now add a new material to budget
+  var m = {
+    type : 'simple',
+    name : 'labor',
+    location : 'California',
+    authority : 'AHB',
+    price : 7.5,
+    units : 'h'
+  };
+  budget.addMaterial(m);
+
+  // now let's add the material to our operation
+  var op = budget.getOperation()[0];
+
+  // adding 3 hours of labor
+  op.addRequiredMaterial(m.name, 3, 'h');
+  console.log(sdk.getTotal());
+
+
+  // now add another date for operation;
+  op.schedule('2018-06-06', 1, 'month');
+  console.log(sdk.getTotal());
 }
